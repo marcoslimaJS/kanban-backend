1cfaadaf2dad54c76cc003b66a2bb29491e0c382
Marcos Lima <socram.oluap2003@gmail.com>
2023-05-16 13:36:34 -0300

    mudando a porta

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(3000, '0.0.0.0', () => {
+app.listen(5000, () => {
   console.log('Server started at http://localhost:3000');
 });
 
