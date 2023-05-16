ed69b6ddd93b364364e0f106a31a4e87066a7cc0
Marcos Lima <socram.oluap2003@gmail.com>
2023-05-15 21:27:03 -0300

    ajustando backend para deploy

--- src/index.js
+++ src/index.js
@@ -9,4 +9,7 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server started at http://localhost:3000');
+});
+
