c9199a8429a8d370b2d80f96dc0c44a3a329a043
Marcos Lima <socram.oluap2003@gmail.com>
2023-05-16 20:15:32 -0300

    .

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ const express = require('express');
 require('express-async-errors');
 const routes = require('./routes');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(routes);
 
 app.use("/", (req, res) => {
   console.log("Bem vindo a api do Kanban");
 });
+app.use(routes);
 
 app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
   console.log('Server started at http://localhost:3000');
